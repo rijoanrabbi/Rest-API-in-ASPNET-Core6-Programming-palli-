ac64f06238ba28497bceaa784fbeb14e63bb7e4b
rijoanrabbi <rijoanrabbi@gmail.com>
2023-03-23 00:12:59 +0600

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet6WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]//you need to use rootpath/api/controllerName
+    //https://localhost:7198/api/Values
+    [ApiController]//mention this is MVC controller or API controller
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]//this called atrribute, which type of request we are use in API, get,put,post,delete.
+        public string GetName()
+        {
+            return "welcome to new lesson";
+        }
+
+    }
+}
